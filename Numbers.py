65b0c44e9d424c6a68e368501468eb965ca9ef10
TheFakeDazz <109765300+TheFakeDazz@users.noreply.github.com>
2022-08-14 11:04:10 -0700

    Update Numbers.py

--- Numbers.py
+++ Numbers.py
@@ -44,14 +44,13 @@ def prime_numbers():
                 return random.choice(mylist)
 # ----------------------------------Next Prime Number Generator (1-100)------------------------------------------------------------------------------------------------
 def prime_generator():
-    mylist = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
-    print('\n'*10)
+    mylist = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97])
     while True:
+        print('\n')
         if len(mylist) == 0:
             print('Out of numbers!')
             break
         ask = input('Would you like to see the next prime number? (Type "yes" or "no"): ')
-        print('\n'*100)
         if ask == 'yes':
             print(mylist.pop(0))
         elif ask == 'no':
