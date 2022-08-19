aa0d1ae9866e20427f42c9798e27757a45b22ce3
TheFakeDazz <109765300+TheFakeDazz@users.noreply.github.com>
2022-08-18 20:01:25 -0700

    Update Coin_Flip_Simulator.py

--- Coin_Flip_Simulator.py
+++ Coin_Flip_Simulator.py
@@ -1,15 +1,15 @@
-import random
-import collections
+from random import choice
+from collections import Counter
 
 
 def coin_flipper(num):
     choices = ['Heads', 'Tails']
     mylist = []
     for w in range(num):
-        x = random.choice(choices)
+        x = choice(choices)
         print(f'Result: {x}')
         mylist.append(x)
-    d = collections.Counter(mylist)
+    d = Counter(mylist)
     print(f'Number of Heads: {d["Heads"]}')
     print(f'Number of Tails: {d["Tails"]}')
     
