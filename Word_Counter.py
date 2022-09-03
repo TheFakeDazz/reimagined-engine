d3d16d6a7f3b5b5a2768d5d2894472d512c14366
TheFakeDazz <109765300+TheFakeDazz@users.noreply.github.com>
2022-09-03 12:50:17 -0700

    Update Word_Counter.py

--- Word_Counter.py
+++ Word_Counter.py
@@ -2,6 +2,8 @@ def word_count():
     while True:
         try:
             namee = input('Enter the full name of your file: ')
+            if namee == 'quit':
+                break
             open_file = open(namee)
             read_file = open_file.read()
             replace_new = read_file.replace('\n', " ")
